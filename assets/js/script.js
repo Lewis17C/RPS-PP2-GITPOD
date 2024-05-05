7ba8ae1d867b3fbaf30860aada2a367003f4872a
Lewis Cunningham <lewiscunningham2001@hotmail.com>
2024-05-05 19:07:46 +0000

    added displayWinner function, debug

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,16 +2,17 @@ const choices = ["rock", "paper", "scissors"]
 let playersScore = 0;
 let computersScore = 0;
 
+//creates the ability for the player to slect an option
 function playersChoice() {
     let playersChoice = getElementById("button");
     playersChoice.addEventListener("click", playGame());
 }
-
+//generates a random choice and assigns it to the computer
 function computersChoice() {
     const computersChoice = Math.floor(Math.random() * choices.length);
     return computersChoice
 }
-
+//creates parameters which dictate the winner of the game
 function playGame(playersChoice) {
     if (playersChoice === computersChoice) {
         return "Tie! Play again!"
@@ -26,9 +27,10 @@ function playGame(playersChoice) {
     computerScore++;
 } 
 }
-
-function displayWinner() {
-
+//displays the winner of the round 
+function displayWinner("display-game-winner") {
+    const winner = document.getElementById("display-game-winner");
+    resultElement.textContent = winner;
 }
 
 function playersScore() {
